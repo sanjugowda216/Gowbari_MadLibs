f325c576bf12192a08e9c49464ff61bdca6567a1
Sanjana Gowda <spgowda216@gmail.com>
2025-09-29 14:07:51 -0700

    ur mom

--- Gowbari_MadLibs.java
+++ Gowbari_MadLibs.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Gowbari_MadLibs{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a noun: ");
+        String noun1 = scanner.nextLine();
+        System.out.print("tjis is a nonun" + noun1);
+        scanner.close();
+    }
+}
