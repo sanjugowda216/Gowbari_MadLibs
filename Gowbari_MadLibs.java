b98afdd768182bf85b71d2fb63cc878f509bdd7b
vaidehiakbari1 <vaidehiakbari@gmail.com>
2025-09-29 14:14:22 -0700

    terlelero

--- Gowbari_MadLibs.java
+++ Gowbari_MadLibs.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 public class Gowbari_MadLibs{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a noun: ");
+        System.out.println("Enter a name: ");
         String noun1 = scanner.nextLine();
-        System.out.print("tjis is a nonun" + noun1);
+        System.out.println("Enter a name of a place: ");
+        String noun2 = scanner.nextLine();
+        System.out.println("Enter another name of a place: ");
+        String noun3 = scanner.nextLine();
+        System.out.println("Enter a verb: ");
+        String verb1 = scanner.nextLine();
+        System.out.println("Enter an action that ends with -ing: ");
+        String verb2 = scanner.nextLine();
+        System.out.println("Enter a adjective: ");
+        String adj1 = scanner.nextLine();
+        System.out.println("Enter another adjective: ");
+        String adj2 = scanner.nextLine();
+        System.out.print("This is a nonun" + noun1);
         scanner.close();
     }
 }
