623a43c87fda452af47d78d4d126b7297362408e
Sanjana Gowda <spgowda216@gmail.com>
2025-09-29 14:24:13 -0700

    done

--- Gowbari_MadLibs.java
+++ Gowbari_MadLibs.java
@@ -8,7 +8,7 @@ public class Gowbari_MadLibs{
         String noun2 = scanner.nextLine();
         System.out.println("Enter another name of a place: ");
         String noun3 = scanner.nextLine();
-        System.out.println("Enter a verb: ");
+        System.out.println("Enter a verb not ending in -ing: ");
         String verb1 = scanner.nextLine();
         System.out.println("Enter an action that ends with -ing: ");
         String verb2 = scanner.nextLine();
@@ -16,7 +16,9 @@ public class Gowbari_MadLibs{
         String adj1 = scanner.nextLine();
         System.out.println("Enter another adjective: ");
         String adj2 = scanner.nextLine();
-        System.out.println("Welcome to the land of Brainrot "+" to "+" where they meet their "+"friend TralaleroTralala")
+        System.out.println("Enter a food: ");
+        String food = scanner.nextLine();
+        System.out.println("Welcome to the land of Brainrot, where " + noun1 + " escapes from " + noun2 + " and travels through the void of " + noun3 + ", all while " + verb2 + " at a speed of 67 thoughts per second. Suddenly, they " + verb1 + " with a " + adj1 + ", " + adj2 + " creature named TralaleroTralala. Nothing makes sense anymore. The sky is "+food+". Reality is optional.");
         scanner.close();
     }
 }
