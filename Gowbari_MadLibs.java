69047e821f5371249f830da2d6cf7867d934901e
vaidehiakbari1 <vaidehiakbari@gmail.com>
2025-09-30 13:37:42 -0700

    conrbal

--- Gowbari_MadLibs.java
+++ Gowbari_MadLibs.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Gowbari_MadLibs{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the land of Brainrot, where ____ escapes from ____ and travels through the void of ____, all while ____ at a speed of 67 thoughts per second. Suddenly, they ____ with a ____, ____ creature named TralaleroTralala. Nothing makes sense anymore. The sky is ____. Reality is optional."); //original story printed out
         // 8 input options
         System.out.println("Enter a name: ");
         String noun1 = scanner.nextLine();
