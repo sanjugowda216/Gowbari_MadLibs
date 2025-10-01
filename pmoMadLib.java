42980cd998196756dd1bb0fae2fb5688695cbdc1
vaidehiakbari1 <vaidehiakbari@gmail.com>
2025-10-01 15:00:25 -0700

    ovo

--- pmoMadLib.java
+++ pmoMadLib.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class Gowbari_MadLibs{
+public class pmoMadLib{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the land of Brainrot, where ____ escapes from ____ and travels through the void of ____, all while ____ at a speed of 67 thoughts per second. Suddenly, they ____ with a ____, ____ creature named TralaleroTralala. Nothing makes sense anymore. The sky is ____. Reality is optional."); //original story printed out
-        System.out.println("Enter a name: ");
-        String noun1 = scanner.nextLine();
+        //System.out.println("Welcome to the land of Brainrot, where ____ escapes from ____ and travels through the void of ____, all while ____ at a speed of 67 thoughts per second. Suddenly, they ____ with a ____, ____ creature named TralaleroTralala. Nothing makes sense anymore. The sky is ____. Reality is optional."); //original story printed out
+        System.out.println("Enter every noun as 'noun,' all adjectives as 'adj,' and all verbs as 'verb'. \nEntrega su MadLib ahora: "); 
+        String imMad = scanner.nextLine();
         scanner.close();
     }
 }
