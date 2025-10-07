2bc9c74e3123323e215cc453d5097ecc7986749a
sanjugowda216 <spgowda216@gmail.com>
2025-10-07 14:02:47 -0700

    done

--- pmoMadLib.java
+++ pmoMadLib.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class pmoMadLib{
-    public static String madLib(String lib){
-        
-        Scanner sc = new Scanner(System.in);
+    public static String madLib(String lib, Scanner sc){
+        System.out.println(lib);
         System.out.print("Enter a noun: ");
         String inputNoun = sc.nextLine();
         System.out.print("Enter a verb: ");
@@ -12,14 +11,19 @@ public class pmoMadLib{
         lib = lib.replace("<noun>", inputNoun);
         lib = lib.replace("<verb>", inputVerb);
         lib = lib.replace("<adj>", inputAdj);
-        sc.close();
+        
         return lib;
  
     }
     // testing function
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
         String test1 = "The <noun> <verb> over the <adj> barn.";
-        System.out.println(madLib(test1));
+        String funTest = "The <noun> <verb> to Brainrot Land in <adj> TralaleroTralala.";
+        String stringo = "<noun> meets its <adj> mom and <verb> away!";
+        System.out.println(madLib(test1, sc));
+        System.out.println(madLib(funTest, sc));
+        System.out.println(madLib(stringo, sc));
+        sc.close();
     }
 }
